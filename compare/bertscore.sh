8ef59e2531a4fc7531002cffd3546f27eadb8ec9
PerceptronV <neutrinovs@gmail.com>
2022-05-24 17:35:02 +0100

    BLEU analysis

---
+++ compare/bertscore.sh
@@ -0,0 +1,16 @@
+bert-score \
+  -r "../data/anki_parallel/spa_test.txt" \
+  -c "../experiments/texts/model_simplification/test_eng2spa_n1_lmse_u128_arelu_e150_b64_d0.1.txt" \
+  --lang es
+
+bert-score \
+  -r "../data/anki_parallel/spa_test.txt" \
+  -c "../seq2seq/seq_outs/0.1/eng2spa_test_full.txt" \
+  --lang es
+
+bert-score \
+  -r "../data/anki_parallel/spa_test.txt" \
+  -c "../seq2seq/seq_outs/1.0/eng2spa_test_full.txt" \
+  --lang es
+
+# ../seq2seq/seq_outs/eng2spa_test_full.txt
--- compare/bleu.py
+++ compare/bleu.py
@@ -1,6 +1,8 @@
 from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
+from matplotlib import pyplot as plt
+from statistics import stdev, mean, median, mode
 from tqdm import tqdm
-from statistics import stdev, mean
+import os
 
 import argparse
 
@@ -11,6 +13,8 @@ parser.add_argument('-a', '--hypA', type=str, required=True,
                     help='path to hypothesis A file')
 parser.add_argument('-b', '--hypB', type=str, required=True,
                     help='path to hypothesis B file')
+parser.add_argument('-d', '--histdir', type=str, required=False, default="./histogram",
+                    help='directory to store histogram')
 
 def chunk2sents(chunk):
   chunk = chunk.strip()
@@ -34,11 +38,32 @@ def multi_sentence_bleu(refs, hyps):
     ret.append(sentence_bleu(r, h))
   return ret
 
-def stats(m_bleus, c_val):
-  ret  = f"mico-average mean  {c_val}\n"
-  ret += f"macro-average mean {mean(m_bleus)}\n"
-  ret += f"standard deviation {stdev(m_bleus)}\n"
+def count(l, func):
+  ret = 0
+  for i in l:
+    if func(i):
+      ret += 1
   return ret
+
+def stats(m_bleus, c_val):
+  ret  = f" mico-average mean | {c_val}\n"
+  ret += f"macro-average mean | {mean(m_bleus)}\n"
+  ret += f"            median | {median(m_bleus)}\n"
+  ret += f"              mode | {mode(m_bleus)}\n"
+  ret += f"standard deviation | {stdev(m_bleus)}\n"
+  ret += f"           # == 0s | {count(m_bleus, lambda x : x == 0)}\n"
+  ret += f"         # <= 0.1s | {count(m_bleus, lambda x : x < 0.1)}\n"
+  ret += f"         # <= 0.4s | {count(m_bleus, lambda x : x <= 0.4)}\n"
+  ret += f"          # > 0.4s | {count(m_bleus, lambda x : x > 0.4)}\n"
+  return ret
+
+def histogram(vals, title, fname, bins=150):
+  plt.hist(vals, bins=bins)
+  plt.title(title)
+  plt.xlim([0, 1])
+  plt.ylim([0, 200])
+  plt.show()
+  plt.savefig(os.path.join(args.histdir, fname+'.png'))
 
 if __name__ == "__main__":
 
@@ -63,3 +88,6 @@ if __name__ == "__main__":
   print(stats(hypA_multi_bleus, hypA_corpus_bleu_val))
   print(f"Hypothesis B BLEU score: (from file at {args.hypB}):")
   print(stats(hypB_multi_bleus, hypB_corpus_bleu_val))
+
+  histogram(hypA_multi_bleus, f"HypA BLEU Distibution ({args.hypA})", "hypA")
+  histogram(hypB_multi_bleus, f"HypB BLEU Distibution ({args.hypB})", "hypB")
